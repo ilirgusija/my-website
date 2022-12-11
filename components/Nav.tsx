7981af4177280494e8ffa37687196e3f73d56169
Ilir Gusija <ilirgusija@gmail.com>
2022-12-11 18:10:46 -0500

    Initialized app, implemented nav bar

---
+++ components/Nav.tsx
@@ -0,0 +1,43 @@
+import Link from 'next/link'
+import { AiFillGithub, AiFillLinkedin, AiFillInfoCircle, AiFillHome } from 'react-icons/ai'
+import { FC } from 'react'
+import Home from '../pages';
+
+
+
+const Nav = () => {
+    return (
+        <div className="fixed top-0 h-screen w-16 m-0 justify-center
+                    flex flex-col
+                    bg-gray-900 text-white shadow-lg">
+            <Link href={"/"}>
+                <NavBarIcon icon={<AiFillHome size={28} />} text={"Home"}/>
+            </Link>
+            <Link href={"/about-me"}>
+                <NavBarIcon icon={<AiFillInfoCircle size={28} />} text="About Me" />
+            </Link>
+            <a href="https://github.com/ilirgusija" target={"_blank"}>
+                <NavBarIcon icon={<AiFillGithub size={28} />} text="My Github" />
+            </a>
+            <a href="https://www.linkedin.com/in/ilir-gusija/" target="_blank">
+                <NavBarIcon icon={<AiFillLinkedin size={28} />} text="My LinkedIn" />
+            </a>
+        </div>
+    )
+}
+
+type IconProps = {
+    icon: JSX.Element;
+    text: string;
+};
+
+const NavBarIcon: FC<IconProps> = ({ icon, text = 'tooltip' }) => (
+    <div className='sidebar-icon group'>
+        {icon}
+        <span className='sidebar-tooltip group-hover:scale-100'>
+            {text}
+        </span>
+    </div>
+);
+
+export default Nav;
---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import Nav from '../components/Nav'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className='flex'>
+      <Head>
+        <title>Ilir Gusija's Website</title>
+        <meta name='keywords' content="Ilir Gusija's portfolio" />
+      </Head>
+      <Nav/>
+      <h1>Welcome to Next</h1>
+    </div>
+  )
+}
